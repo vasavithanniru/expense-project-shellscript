e0f0c87429c42aee170b2cbba8a8d9de3d773816
vasavi <tvasu370@gmail.com>
2025-07-31 09:20:35 +0530

    mysql

--- mysql.sh
+++ mysql.sh
@@ -23,6 +23,7 @@ VALIDATE(){
     if [ $1 -ne 0 ]
     then    
         echo -e "$2 is....$R FAILED $N" | tee -a $LOG_FILE
+        exit 1
     else
         echo -e "$2 is...$G SUCCESS $N"  | tee -a $LOG_FILE
     fi     
