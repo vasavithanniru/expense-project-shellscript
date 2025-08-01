5d3e9bf93115beeb509a870eed52b841d23c7f0e
vasavi <tvasu370@gmail.com>
2025-08-01 12:22:07 +0530

    scripts

---
+++ backend.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+LOG_FOLDER=/var/log/expense
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+TIME_STAMP=$(date +%F-%H-%M-%S)
+LOG_FILE="$LOG_FOLDER/$SCRIPT_NAME-$TIME_STAMP.log"
+
+USERID=$(id -u)
+if [ $USERID -ne 0 ]
+then
+    echo "Run with root prevellege"
+    exit 1
+fi
+
+VALIDATE(){
+if [ $1 -ne 0 ]
+then
+    echo "$2 is...FAILED"
+    exit 1
+else
+    echo "$2 is...SUCCESS"
+fi 
+}
+
+dnf module disable nodejs -y  &>>$LOG_FILE
+VALIDATE $? "Disabling Nodejs"
+
+dnf module enable nodejs:20 -y &>>$LOG_FILE
+VALIDATE $? "Enabling Nodejs 20"
+
+dnf install nodejs -y &>>$LOG_FILE
+VALIDATE $? "Installing Nodejs"
+
+id expense #to check expense user is exists or not
+
+if [ $? -ne 0 ]
+then 
+    echo "Expense user is not exists..Creating user"
+    useradd expense
+else 
+    echo "Expense user is already exists..SKIPPING" 
+fi 
+
+mkdir -p /app &>>$LOG_FILE
+curl -o /tmp/backend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-backend-v2.zip &>>$LOG_FILE
+VALIDATE $? "Downloding the backend code"
+
+cd /app &>>$LOG_FILE
+unzip /tmp/backend.zip &>>$LOG_FILE
+VALIDATE $? "unzipping the backend code"
+
+npm install &>>$LOG_FILE
+VALIDATE $? "Installing depensencies"
+
+
+       
+
