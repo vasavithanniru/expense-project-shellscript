57fb5770a416e7c6e17cf8a5df49e43d89675af8
vasavi <tvasu370@gmail.com>
2025-08-02 08:42:19 +0530

    expense-project

--- frontend.sh
+++ frontend.sh
@@ -10,39 +10,43 @@ USERID=$(id -u)
 
 if [ $USERID -ne 0 ]
 then    
-    echo "Run with root prevellege"
+    echo "Run with root prevellege" | tee -a $LOG_FILE
     exit 1
 fi
 
 VALIDATE(){
 if [ $1 -ne 0 ]
 then
-    echo "$2 is...FAILED..Check it"
+    echo "$2 is...FAILED..Check it"  | tee -a $LOG_FILE
     exit 1
 else
-    echo "$2 is...SUCCESS"
+    echo "$2 is...SUCCESS" | tee -a $LOG_FILE
 fi  
 }
 
-dnf install nginx -y
+dnf install nginx -y  &>>$LOG_FILE
 VALIDATE $? "Nginx installation"
 
-systemctl enable nginx -y
+systemctl enable nginx -y   &>>$LOG_FILE
 VALIDATE $? "Enabling nginx"     
 
-systemctl start nginx -y
+systemctl start nginx -y   &>>$LOG_FILE
 VALIDATE $? "Starting nginx"
 
-rm -rf /usr/share/nginx/html/*
+rm -rf /usr/share/nginx/html/*  &>>$LOG_FILE
 VALIDATE $? "Removing default content"
 
-curl -o /tmp/frontend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-frontend-v2.zip
+curl -o /tmp/frontend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-frontend-v2.zip  &>>$LOG_FILE
 VALIDATE $? "Downloading frontend code"
 
-cd /usr/share/nginx/html
-unzip /tmp/frontend.zip
+cd /usr/share/nginx/html 
+unzip /tmp/frontend.zip  &>>$LOG_FILE
 VALIDATE $? "Unzipping frontend code"
 
+cp /home/ec2-user/expense-project-shellscript/expense.conf /etc/nginx/default.d/expense.conf  &>>$LOG_FILE
+VALIDATE $? "Copying frontend code" 
 
+systemctl restart nginx  &>>$LOG_FILE
+VALIDATE $? "Restarting Nginx"
 
 
